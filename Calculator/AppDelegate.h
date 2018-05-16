0a81eb3d665988d8665cf5a6b0adfd3eac72956e
Ushio Cheng <ushio.cheng@gmail.com>
2018-05-15 18:21:37 -0700

    Initial Commit

---
+++ Calculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Calculator
+//
+//  Created by Ushio Cheng on 2018/5/15.
+//  Copyright © 2018年 Ushio Cheng. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Calculator/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Calculator
+//
+//  Created by Ushio Cheng on 2018/5/15.
+//  Copyright © 2018年 Ushio Cheng. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}

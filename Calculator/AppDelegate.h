0a5746fadb31b0ba2920f332c53e5d4780f04761
Ushio Cheng <ushio.cheng@gmail.com>
2018-05-16 18:02:20 -0700

    Update Header Comment

--- Calculator/AppDelegate.h
+++ Calculator/AppDelegate.h
@@ -5,11 +5,16 @@
 //  Created by Ushio Cheng on 2018/5/15.
 //  Copyright © 2018年 Ushio Cheng. All rights reserved.
 //
-
+/*!
+ @header AppDelegate.h
+ @brief application delegate
+ @author Ushio Cheng
+ @version 0.0.1
+ @remark N/A
+ */
 #import <Cocoa/Cocoa.h>
 
 @interface AppDelegate : NSObject <NSApplicationDelegate>
 
 
 @end
-

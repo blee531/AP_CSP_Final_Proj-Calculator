479ca8a6f2e9ac199e4b39fa292e770a40ea1391
Ushio Cheng <ushio.cheng@gmail.com>
2018-05-16 18:27:37 -0700

    Create function and part of eval function

--- Calculator/Calculator.h
+++ Calculator/Calculator.h
@@ -5,9 +5,19 @@
 //  Created by Ushio Cheng on 2018/5/15.
 //  Copyright © 2018年 Ushio Cheng. All rights reserved.
 //
-
+/*!
+ @header Calculator.h
+ @brief a class use to convert string to expression
+ @author Ushio Cheng
+ @version 0.0.1
+ @remark N/A
+ */
 #import <Foundation/Foundation.h>
 
 @interface Calculator : NSObject
+
+-(double)evaluateExpression:(NSString *) expression;
+
+
 
 @end
--- Calculator/Calculator.m
+++ Calculator/Calculator.m
@@ -10,4 +10,20 @@
 
 @implementation Calculator
 
+
+
+-(double)evaluateExpression:(NSString *) expression{
+    NSMutableArray * charList = [[NSMutableArray alloc] initWithCapacity:@%d, (expression.length)];
+    NSMutableArray * operatorPosition = [[NSMutableArray alloc] initWithCapacity:@1];
+    for(NSUInteger pointer = 0;pointer<expression.length;pointer++){
+        char charAtPointer = [expression characterAtIndex:i]
+        [charList replaceObjectAtIndex:pointer withObject:charAtPointer];
+        if(charAtPointer=='+'||charAtPointer=='-'||charAtPointer=='*'||charAtPointer=='/'){
+            [operatorPosition addObject:pointer];
+        }
+    }
+    NSMutableDictionary * operationList = [[NSMutableDictionary alloc] initWithCapacity:@1];
+    [operationList setObject:(ObjectType)anObject forKey:operatorPosition[pointer]];
+}
+
 @end
